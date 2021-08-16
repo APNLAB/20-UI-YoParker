f24af6ef65a058247b2348d900b5611d60962dc8
Heshan <heshan.imedha@gmail.com>
2021-08-16 20:26:46 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/example/a20uiyoparker/Splash.java
@@ -0,0 +1,33 @@
+package com.example.a20uiyoparker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+
+    ImageView splashScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        splashScreen = findViewById(R.id.splashScreen);
+
+//        splashScreen.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Splash.this, Locations.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
