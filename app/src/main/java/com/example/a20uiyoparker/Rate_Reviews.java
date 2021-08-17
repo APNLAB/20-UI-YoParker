66ad6bef976e88ac7efd616f29355c51ed0d38f5
Heshan <heshan.imedha@gmail.com>
2021-08-17 13:06:15 +0530

    Rate & Reviews Screen Added

---
+++ app/src/main/java/com/example/a20uiyoparker/Rate_Reviews.java
@@ -0,0 +1,14 @@
+package com.example.a20uiyoparker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rate_Reviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rate_reviews);
+    }
+}
