dbeb1adf6050691775b61557702b4478d6ad4ace
Heshan <heshan.imedha@gmail.com>
2021-08-26 16:53:00 +0530

    Date Time Popup Added

--- app/src/main/java/com/example/a20uiyoparker/Details.java
+++ app/src/main/java/com/example/a20uiyoparker/Details.java
@@ -1,8 +1,12 @@
 package com.example.a20uiyoparker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +19,25 @@ public class Details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details);
 
+        Dialog dialog;
+
+        dialog = new Dialog(Details.this);
+
         Button overView= findViewById(R.id.overView);
         Button start= findViewById(R.id.start);
+        CardView timePickerCard = findViewById(R.id.timePickerCard);
+
+        dialog.setContentView(R.layout.time_picker_popup);
+
+        Button cancelBtn = dialog.findViewById(R.id.cancelBtn);
+
+//        cancelBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Details.this, Splash.class));
+//                //dialog.dismiss();
+//            }
+//        });
 
         overView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +52,26 @@ public class Details extends AppCompatActivity {
                 startActivity(new Intent(Details.this, Start.class));
             }
         });
+
+
+
+        timePickerCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.setContentView(R.layout.time_picker_popup);
+                
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.show();
+
+
+
+            }
+        });
+
+
+
+
     }
 }
+
