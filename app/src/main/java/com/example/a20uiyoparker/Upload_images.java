7bacc772570bb08edc7c30bfed01ad1fb8cdea35
Malith Wijekoon <malithcw89@gmail.com>
2021-09-16 14:22:19 +0530

    Add Upload images Screen

---
+++ app/src/main/java/com/example/a20uiyoparker/Upload_images.java
@@ -0,0 +1,14 @@
+package com.example.a20uiyoparker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Upload_images extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.upload_images);
+    }
+}
